d5e2529cc46237b8a239a44666b7b7ef4849b58b
Josafath Fuentes <josafm8@gmail.com>
2024-11-26 12:48:42 -0600

    iniciando en los ciclos

---
+++ ciclos.py
@@ -0,0 +1,15 @@
+###################
+# bucles, loops o ciclos es estructura de control en programacion que permite ejecutar una o varias lines de codigo multiples veces
+
+
+# for - se usa cuando sabemos cuantas veces se debe de repetir ciertas instrucciones
+# i es una variable de control, esta se actualiza automaticamente antes de cada iteracion
+#start para inicializar la variable, stop para detenerla y step para cuanto se le va a sumar
+for i in range(5):
+    print(i)
+
+for n in range(5, 100, 5): #start, stop, step
+    print(n)
+
+
+#Ciclos iterables, que iteran sobre string, listas, tuplas, diccionarios, etc
